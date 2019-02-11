d8e6842f5ee719720d20d2da8ec767a7b66f1ae8
WybeTuring <Karllemfon@gmail.com>
2019-02-11 13:51:43 +0000

    Euler 25 with help

---
+++ projecteuler25.py
@@ -0,0 +1,9 @@
+def fibonacci(n):
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return (fibonacci(n-1) + fibonacci(n-2))
+# writing a function that will return the first
+while len(str(n)) != 3:
+    n += 1
+return n
